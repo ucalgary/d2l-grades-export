80c3c4ad83e9ba68cfb4f60e3493bef5e0014522
King Chung Huang <kinghuang@mac.com>
2014-03-19 10:05:33 -0600

    Corrected decoding of the application config.

--- js/gradesexport.js
+++ js/gradesexport.js
@@ -26,7 +26,7 @@
 
 	GradesExport.init = function() {
 		if (typeof(GradesExport.config) == 'string') {
-			GradesExport.config = JSON.parse(CryptoJS.AES.decrypt(GradesExport.config, document.URL.split('?')[0]));
+			GradesExport.config = JSON.parse(CryptoJS.AES.decrypt(GradesExport.config, document.URL.split('?')[0]).toString(CryptoJS.enc.Utf8));
 		}
 		this.appContext = new D2L.ApplicationContext('localhost', GradesExport.config.appId, GradesExport.config.appKey);
 		this.userContext = GradesExport.appContext.createUserContext(this.config.scheme + '://' + this.config.host, 443, window.location.href);
